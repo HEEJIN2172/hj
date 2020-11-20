9edd0f0e75bb31bd9e688b1e06d1a5244500e73e
HEEJIN2172 <qkrgmlwls063@gmail.com>
2020-11-20 15:18:31 +0900

    port setting

--- start.js
+++ start.js
@@ -5,15 +5,15 @@ const http    = require('http');
 const express = require('express');
 const server  = express();
 
-<<<<<<< HEAD
-const serverPort = 80;
+//<<<<<<< HEAD
+const serverPort = 2172;
 //port 자신거에 맞게 수정하기
-=======
-const serverPort = 1948;
+
+//const serverPort = 1948;
 //1234
->>>>>>> f968c59804416f6bccbf7afcaa03332807f46ce6
+//>>>>>>> f968c59804416f6bccbf7afcaa03332807f46ce6
 
-
+//
 /************* Routing **************/
 //웹페이지의 세부 주소를 지정한다.
 server.use('/',            require('./server'));
